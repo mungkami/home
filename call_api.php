4ae366009bcb3474e083c6b99f5becb148548aba
김영규 <challengingyu@gmail.com>
2015-03-25 17:51:54 +0900

    test

--- call_api.php
+++ call_api.php
@@ -19,108 +19,117 @@ try {
 	$dbh = $database->getDbh();
 	$dbh->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND, "SET NAMES euckr");
 
-	$query = "SELECT paymentIdx FROM pay_data ORDER BY idx DESC LIMIT 1 ";
-	$stmt = $dbh->prepare($query);
-	$stmt->execute();
-	$payData = $stmt->fetch(PDO::FETCH_ASSOC);
-	$paymentIdx = $payData['paymentIdx'];
+	#insertQuery ¼³Á¤
+	$insertQuery  = "INSERT INTO pay_data ( paymentIdx, payTableKey, payMethod, payAmount, ";
+	$insertQuery .= "payStatus, serviceName, productCode, parentId, userId, orderName, orderNo, ";
+	$insertQuery .= "pgId, tradeNo, approvalNo, bankName, accountNo, accountName, escrowYN, escrowNo, ";
+	$insertQuery .= "mobileNo, regDate, bankCheckIdx, updateDate, testFlag, refundIdx, cancelKey, ";
+	$insertQuery .= "managerId, devRemark, remark) VALUES( ";
+	$insertQuery .= ":paymentIdx, :payTableKey, :payMethod, :payAmount, ";
+	$insertQuery .= ":payStatus, :serviceName, :productCode, :parentId, :userId, :orderName, :orderNo, ";
+	$insertQuery .= ":pgId, :tradeNo, :approvalNo, :bankName, :accountNo, :accountName, :escrowYN, :escrowNo, ";
+	$insertQuery .= ":mobileNo, :regDate, :bankCheckIdx, :updateDate, :testFlag, :refundIdx, :cancelKey, ";
+	$insertQuery .= ":managerId, :devRemark, :remark )";
+
+	while(true){
+
+		$query = "SELECT paymentIdx FROM pay_data ORDER BY idx DESC LIMIT 1 ";
+		$stmt = $dbh->prepare($query);
+		$stmt->execute();
+		$payData = $stmt->fetch(PDO::FETCH_ASSOC);
+		$paymentIdx = $payData['paymentIdx'];
 
 
 
-	$getDataUrl  = 'https://billingadmin.cafe24.com/test/ykkim02/test_get_pay_data.php';
-	$getDataUrl .= '?paymentIdx='.$paymentIdx;
+		$getDataUrl  = 'https://billingadmin.cafe24.com/test/ykkim02/test_get_pay_data.php';
+		$getDataUrl .= '?paymentIdx='.$paymentIdx;
 
-	$sslFlag = Net::getSSLFlag( $getDataUrl );
+		$sslFlag = Net::getSSLFlag( $getDataUrl );
 
-	$getData = Net::getHtml( $getDataUrl, 'GET', 30, TRUE );
-	$getData = unserialize($getData);
+		$getData = Net::getHtml( $getDataUrl, 'GET', 30, TRUE );
+		$getData = unserialize($getData);
 
-	echo 'last paymentIdx='.$paymentIdx.PHP_EOL;
+		echo 'last paymentIdx='.$paymentIdx.PHP_EOL;
+
+		if( empty( $getData ) == true ){
+			echo 'END '.PHP_EOL;
+			exit;
+		}
 
 
-	$query  = "INSERT INTO pay_data ( paymentIdx, payTableKey, payMethod, payAmount, ";
-	$query .= "payStatus, serviceName, productCode, parentId, userId, orderName, orderNo, ";
-	$query .= "pgId, tradeNo, approvalNo, bankName, accountNo, accountName, escrowYN, escrowNo, ";
-	$query .= "mobileNo, regDate, bankCheckIdx, updateDate, testFlag, refundIdx, cancelKey, ";
-	$query .= "managerId, devRemark, remark) VALUES( ";
-	$query .= ":paymentIdx, :payTableKey, :payMethod, :payAmount, ";
-	$query .= ":payStatus, :serviceName, :productCode, :parentId, :userId, :orderName, :orderNo, ";
-	$query .= ":pgId, :tradeNo, :approvalNo, :bankName, :accountNo, :accountName, :escrowYN, :escrowNo, ";
-	$query .= ":mobileNo, :regDate, :bankCheckIdx, :updateDate, :testFlag, :refundIdx, :cancelKey, ";
-	$query .= ":managerId, :devRemark, :remark )";
+		$stmt = $dbh->prepare($insertQuery);
+		$stmt->bindParam( ':paymentIdx', $paymentIdx, PDO::PARAM_INT );
+		$stmt->bindParam( ':payTableKey', $payTableKey, PDO::PARAM_STR );
+		$stmt->bindParam( ':payMethod', $payMethod, PDO::PARAM_STR );
+		$stmt->bindParam( ':payAmount', $payAmount, PDO::PARAM_INT );
+		$stmt->bindParam( ':payStatus', $payStatus, PDO::PARAM_STR );
+		$stmt->bindParam( ':serviceName', $serviceName, PDO::PARAM_STR );
+		$stmt->bindParam( ':productCode', $productCode, PDO::PARAM_STR );
+		$stmt->bindParam( ':parentId', $parentId, PDO::PARAM_STR );
+		$stmt->bindParam( ':userId', $userId, PDO::PARAM_STR );
+		$stmt->bindParam( ':orderName', $orderName, PDO::PARAM_STR );
+		$stmt->bindParam( ':orderNo', $orderNo, PDO::PARAM_STR );
+		$stmt->bindParam( ':pgId', $pgId, PDO::PARAM_STR );
+		$stmt->bindParam( ':tradeNo', $tradeNo, PDO::PARAM_STR );
+		$stmt->bindParam( ':approvalNo', $approvalNo, PDO::PARAM_STR );
+		$stmt->bindParam( ':bankName', $bankName, PDO::PARAM_STR );
+		$stmt->bindParam( ':accountNo', $accountNo, PDO::PARAM_STR );
+		$stmt->bindParam( ':accountName', $accountName, PDO::PARAM_STR );
+		$stmt->bindParam( ':escrowYN', $escrowYN, PDO::PARAM_STR );
+		$stmt->bindParam( ':escrowNo', $escrowNo, PDO::PARAM_STR );
+		$stmt->bindParam( ':mobileNo', $mobileNo, PDO::PARAM_STR );
+		$stmt->bindParam( ':regDate', $regDate, PDO::PARAM_STR );
+		$stmt->bindParam( ':bankCheckIdx', $bankCheckIdx, PDO::PARAM_INT );
+		$stmt->bindParam( ':updateDate', $updateDate, PDO::PARAM_STR );
+		$stmt->bindParam( ':testFlag', $testFlag, PDO::PARAM_STR );
+		$stmt->bindParam( ':refundIdx', $refundIdx, PDO::PARAM_INT );
+		$stmt->bindParam( ':cancelKey', $cancelKey, PDO::PARAM_STR );
+		$stmt->bindParam( ':managerId', $managerId, PDO::PARAM_STR );
+		$stmt->bindParam( ':devRemark', $devRemark, PDO::PARAM_STR );
+		$stmt->bindParam( ':remark', $remark, PDO::PARAM_STR );
 
-	$stmt = $dbh->prepare($query);
-	$stmt->bindParam( ':paymentIdx', $paymentIdx, PDO::PARAM_INT );
-	$stmt->bindParam( ':payTableKey', $payTableKey, PDO::PARAM_STR );
-	$stmt->bindParam( ':payMethod', $payMethod, PDO::PARAM_STR );
-	$stmt->bindParam( ':payAmount', $payAmount, PDO::PARAM_INT );
-	$stmt->bindParam( ':payStatus', $payStatus, PDO::PARAM_STR );
-	$stmt->bindParam( ':serviceName', $serviceName, PDO::PARAM_STR );
-	$stmt->bindParam( ':productCode', $productCode, PDO::PARAM_STR );
-	$stmt->bindParam( ':parentId', $parentId, PDO::PARAM_STR );
-	$stmt->bindParam( ':userId', $userId, PDO::PARAM_STR );
-	$stmt->bindParam( ':orderName', $orderName, PDO::PARAM_STR );
-	$stmt->bindParam( ':orderNo', $orderNo, PDO::PARAM_STR );
-	$stmt->bindParam( ':pgId', $pgId, PDO::PARAM_STR );
-	$stmt->bindParam( ':tradeNo', $tradeNo, PDO::PARAM_STR );
-	$stmt->bindParam( ':approvalNo', $approvalNo, PDO::PARAM_STR );
-	$stmt->bindParam( ':bankName', $bankName, PDO::PARAM_STR );
-	$stmt->bindParam( ':accountNo', $accountNo, PDO::PARAM_STR );
-	$stmt->bindParam( ':accountName', $accountName, PDO::PARAM_STR );
-	$stmt->bindParam( ':escrowYN', $escrowYN, PDO::PARAM_STR );
-	$stmt->bindParam( ':escrowNo', $escrowNo, PDO::PARAM_STR );
-	$stmt->bindParam( ':mobileNo', $mobileNo, PDO::PARAM_STR );
-	$stmt->bindParam( ':regDate', $regDate, PDO::PARAM_STR );
-	$stmt->bindParam( ':bankCheckIdx', $bankCheckIdx, PDO::PARAM_INT );
-	$stmt->bindParam( ':updateDate', $updateDate, PDO::PARAM_STR );
-	$stmt->bindParam( ':testFlag', $testFlag, PDO::PARAM_STR );
-	$stmt->bindParam( ':refundIdx', $refundIdx, PDO::PARAM_INT );
-	$stmt->bindParam( ':cancelKey', $cancelKey, PDO::PARAM_STR );
-	$stmt->bindParam( ':managerId', $managerId, PDO::PARAM_STR );
-	$stmt->bindParam( ':devRemark', $devRemark, PDO::PARAM_STR );
-	$stmt->bindParam( ':remark', $remark, PDO::PARAM_STR );
+		#getData -> db INSERT
+		foreach( $getData as $key => $row ){
 
-	#getData -> db INSERT
-	foreach( $getData as $key => $row ){
+			$serviceName 	= iconv( 'euc-kr', 'utf-8', $row['serviceName'] );
+			$orderName		= iconv( 'utf-8', 'euc-kr', $row['orderName'] );
+			$bankName		= iconv( 'utf-8', 'euc-kr', $row['bankName'] );
 
-		$serviceName 	= iconv( 'euc-kr', 'utf-8', $row['serviceName'] );
-		$orderName		= iconv( 'utf-8', 'euc-kr', $row['orderName'] );
-		$bankName		= iconv( 'utf-8', 'euc-kr', $row['bankName'] );
+			echo 'paymentIdx='.$key.PHP_EOL;
 
-		echo 'paymentIdx='.$key.PHP_EOL;
+			$paymentIdx	 	= $key;
+			$payTableKey 	= $row['payTableKey'];
+			$payMethod 		= $row['payMethod'];
+			$payAmount		= $row['payAmount'];
+			$payStatus		= $row['payStatus'];
+			$serviceName	= $serviceName;
+			$productCode	= $row['productCode'];
+			$parentId		= $row['parentId'];
+			$userId			= $row['userId'];
+			$orderNo		= $row['orderNo'];
+			$pgId			= $row['pgId'];
+			$tradeNo		= $row['tradeNo'];
+			$approvalNo		= $row['approvalNo'];
+			$accountNo		= $row['accountNo'];
+			$accountName	= $row['accountName'];
+			$escrowYN		= $row['escrowYN'];
+			$escrowNo		= $row['escrowNo'];
+			$mobileNo		= $row['mobileNo'];
+			$regDate		= $row['regDate'];
+			$bankCheckIdx	= $row['bankCheckIdx'];
+			$updateDate		= $row['updateDate'];
+			$testFlag		= $row['testFlag'];
+			$refundIdx		= $row['refundIdx'];
+			$cancelKey		= $row['cancelKey'];
+			$managerId		= $row['managerId'];
+			$devRemark		= $row['devRemark'];
+			$remark			= $row['remark'];
 
-		$paymentIdx	 	= $key;
-		$payTableKey 	= $row['payTableKey'];
-		$payMethod 		= $row['payMethod'];
-		$payAmount		= $row['payAmount'];
-		$payStatus		= $row['payStatus'];
-		$serviceName	= $serviceName;
-		$productCode	= $row['productCode'];
-		$parentId		= $row['parentId'];
-		$userId			= $row['userId'];
-		$orderNo		= $row['orderNo'];
-		$pgId			= $row['pgId'];
-		$tradeNo		= $row['tradeNo'];
-		$approvalNo		= $row['approvalNo'];
-		$accountNo		= $row['accountNo'];
-		$accountName	= $row['accountName'];
-		$escrowYN		= $row['escrowYN'];
-		$escrowNo		= $row['escrowNo'];
-		$mobileNo		= $row['mobileNo'];
-		$regDate		= $row['regDate'];
-		$bankCheckIdx	= $row['bankCheckIdx'];
-		$updateDate		= $row['updateDate'];
-		$testFlag		= $row['testFlag'];
-		$refundIdx		= $row['refundIdx'];
-		$cancelKey		= $row['cancelKey'];
-		$managerId		= $row['managerId'];
-		$devRemark		= $row['devRemark'];
-		$remark			= $row['remark'];
+			$result = $stmt->execute();
 
-		$result = $stmt->execute();
-
-		if( $result == false ){
-			echo 'Insert Error | payTableKey='.$payTableKey.PHP_EOL;
+			if( $result == false ){
+				echo 'Insert Error | payTableKey='.$payTableKey.PHP_EOL;
+			}
 		}
 	}
 
